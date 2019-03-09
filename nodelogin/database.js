220a9e71f0ddd4e798929fb75f2d8521d30d2cbb
Hélder Lopes <aluno19784@ipt.pt>
2019-03-09 15:44:13 +0000

    Backend
    Base de dados + login + api para a tabela dos utilizadores + modulos utilizados.
    
    Co-Authored-By: Edgar Oliveira <aluno19987@users.noreply.github.com>
    Co-Authored-By: andrelfl <andrelfl@users.noreply.github.com>
    Co-Authored-By: dgmoreno <dgmoreno@users.noreply.github.com>
    Co-Authored-By: Diogo Ribeiro <keiriko@users.noreply.github.com>
    Co-Authored-By: miguelangelotome <miguelangelotome@users.noreply.github.com>

---
+++ nodelogin/database.js
@@ -0,0 +1,35 @@
+//modules
+var mysql = require('mysql');
+//coneção com db
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "admin",
+  password: "admin",
+  database: "brandibDB"
+});
+//inicia coneção com db
+con.connect(function(err) {
+	if (err) throw err;
+	console.log("Connected!");
+	//eliminar tabela
+	var sql = "drop table if exists tblUsers;";
+	con.query(sql, function (err, result) {
+		if (err) throw err;
+		console.log("Table dropped");
+	});
+	//criar tabela
+	sql = "create table if not exists tblUsers(id int UNIQUE not null AUTO_INCREMENT, nome varchar(50) not null, username varchar(16) not null, password varchar(50) not null, email varchar(50) not null, tipo varchar(50) not null, primary key(id))ENGINE = InnoDB;";
+	con.query(sql, function (err, result) {
+		if (err) throw err;
+		console.log("Table created");
+	});
+	//inserir registos
+	sql = "INSERT INTO tblUsers (id, nome, username, password, email, tipo) VALUES (1, 'João Nunes', 'joaon', 'pass123', 'jn@hotmail.com', 'aluno'),(2, 'Edgar Oliveira', 'edgaroli', 'pass123', 'eo@hotmail.com', 'aluno'),(3, 'Hélder Lopes', 'hellopes', 'pass123', 'hl@hotmail.com', 'aluno'),(4, 'Miguel Tomé', 'mtome', 'pass123', 'mt@hotmail.com', 'aluno'),(5, 'David Moreno', 'dmoreno', 'pass123', 'dm@hotmail.com', 'aluno'),(6, 'Diogo Ribeiro', 'dribeiro', 'pass123', 'dr@hotmail.com', 'aluno'),(7, 'André Fronteira', 'afronteira', 'pass123', 'af@hotmail.com', 'aluno')";
+	con.query(sql, function (err, result) {
+		if (err) throw err;
+		console.log("Values inserted");
+	});
+//termina coneção
+con.end();
+
+});
---
+++ nodelogin/login.js
@@ -0,0 +1,116 @@
+//modules
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+//coneção com db
+var con = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'admin',
+	password : 'admin',
+	database : 'brandibDB'
+});
+//inicia o express
+var app = express();
+//define sessão
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+//define o bodyparser para trabalhar com json
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+//pagina default
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/login.html'));
+});
+//method: post | action: auth
+//autentica o utilizador através do username e password
+app.post('/auth', function(request, response) {
+	//guarda o username e password recebidos
+	var username = request.body.username;
+	var password = request.body.password;
+	//foram recebidos dados
+	if (username && password) {
+		//procura utilizador na db
+		con.query('SELECT * FROM tblUsers WHERE username = ? AND password = ?', [username, password], 			function(error, results, fields) {
+			//utilizador encontrado
+			if (results.length > 0) {
+				//guarda a informação do utilizador logged in
+				request.session.loggedin = true;
+				request.session.username = username;
+				//redireciona para a página home
+				response.redirect('/home');
+			//utilizador não encontrado
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	//não foram recebidos dados
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+//method: post | action: logout
+//termina a sessão
+app.post('/logout', function(request, response) {
+	//apaga as variaveis de sessão
+	request.session.loggedin = false;
+	request.session.username = null;
+	console.log("User logged out")
+	//redireciona para a página de login
+	response.redirect('/');
+	response.end();
+});
+//pagina home
+app.get('/home', function(request, response) {
+	//utilizador logged in
+	if (request.session.loggedin) {
+		response.sendFile(path.join(__dirname + '/home.html'));
+	//utilizador logged out
+	} else {
+		response.send('Please login to view this page!');
+		response.end();
+	}
+});
+
+//*********************** API **************************//
+
+//lista de utilizadores
+app.get("/users", (req, res) => {
+  let sql = "SELECT * FROM tblUsers";
+
+  con.query(sql, (err, results) => {
+    if (err) {
+      console.error("Erro get users", err);
+      res.status(500).json({ erro: "Erro na query" });
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
+
+//utilizador (pelo ID)
+app.get("/users/:id", (req, res) => {
+  let sql = "SELECT * FROM tblUsers WHERE id = ?";
+
+  // req.params.id mapeia o :id que está no URL acima.
+  con.query(sql, [req.params.id], (err, results) => {
+    if (err) {
+      console.error("Erro get user", err);
+      res.status(500).json({ erro: "Erro na query" });
+    } else {
+      if (results.length ==0) {
+	res.status(404).json({ erro: "User not found" });
+      } else {
+        res.status(200).json(results);
+      }
+    }
+  });
+});
+
+app.listen(8080);
