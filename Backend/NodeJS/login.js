ef5e9ffd454bdb5d8429d13d734caa8f20ef02d3
Diogo Ribeiro <aluno20167@ipt.pt>
2019-03-16 13:24:01 +0000

    Update login.js

--- Backend/NodeJS/login.js
+++ Backend/NodeJS/login.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 //pagina default
 app.get('/', function(request, response) {
-	response.sendFile(path.join(__dirname + '/login.html'));
+	response.sendFile(path.join(__dirname + '/index.html'));
 });
 //method: post | action: auth
 //autentica o utilizador através do username e password
