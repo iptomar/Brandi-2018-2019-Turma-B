45252d1fee9d754c871c9ac6ce0c03370bb34fb5
Diogo Ribeiro <aluno20167@ipt.pt>
2019-03-19 01:33:37 +0000

    Possível solução para a ligação frontend/backend

--- Backend/NodeJS/login.js
+++ Backend/NodeJS/login.js
@@ -19,6 +19,11 @@ app.use(session({
 	resave: true,
 	saveUninitialized: true
 }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 //define o bodyparser para trabalhar com json
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
