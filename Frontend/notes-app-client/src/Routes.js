98f5dc6c8d1b7b369bbb26857e9ace062b14e58b
Andre Fronteira <aluno20160@ipt.pt>
2019-03-26 23:32:45 +0000

    update dos routes

--- Frontend/notes-app-client/src/Routes.js
+++ Frontend/notes-app-client/src/Routes.js
@@ -4,6 +4,11 @@ import Login from "./containers/Login";
 import Profile from "./containers/Profile";
 import Logout from "./containers/Logout";
 import FichaTecnica from "./containers/FichaTecnica";
+import Menu from "./containers/Managing/Menu";
+import Add from "./containers/Managing/Add";
+import Remove from "./containers/Managing/Remove";
+import Consultar from "./containers/Managing/Consultar";
+import Edit from "./containers/Managing/Edit";
 
 export default () =>
   <Switch>
@@ -12,4 +17,9 @@ export default () =>
     <Route path="/profile" exact component={Profile} />
     <Route path="/logout" exact component={Logout} />
     <Route path="/fichatecnica" exact component={FichaTecnica} />
+    <Route path="/menu" exact component={Menu} />
+    <Route path="/add" exact component={Add} />
+    <Route path="/remove" exact component={Remove} />
+    <Route path="/consultar" exact component={Consultar} />
+    <Route path="/edit" exact component={Edit} />
   </Switch>;
