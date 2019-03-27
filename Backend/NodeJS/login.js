fd0cf047fa3226111e30a5146ff002bd9084b3c8
Hélder Lopes <aluno19784@ipt.pt>
2019-03-27 11:35:59 +0000

    API para objetos e tecnicos de um processo

--- Backend/NodeJS/login.js
+++ Backend/NodeJS/login.js
@@ -69,165 +69,215 @@ app.post('/logout', function(request, response) {
 
 //lista de tecnicos
 app.get("/tecnicos", (req, res) => {
-  let sql = "SELECT * FROM tecnicos";
+	let sql = "SELECT * FROM tecnicos";
 
-  con.query(sql, (err, results) => {
-    if (err) {
-      console.error("Erro get tecnicos", err);
-      res.status(500).json({ erro: "Erro na query" });
-    } else {
-      res.status(200).json(results);
-    }
-  });
+	con.query(sql, (err, results) => {
+		if (err) {
+			console.error("Erro get tecnicos", err);
+			res.status(500).json({ erro: "Erro na query" });
+		} else {
+			res.status(200).json(results);
+		}
+	});
 });
 
 //tecnico (pelo ID)
 app.get("/tecnicos/id/:id", (req, res) => {
-  let sql = "SELECT * FROM tecnicos WHERE idTecnico = ?";
+	let sql = "SELECT * FROM tecnicos WHERE idTecnico = ?";
 
-  // req.params.id mapeia o :id que está no URL acima.
-  con.query(sql, [req.params.id], (err, results) => {
-    if (err) {
-      console.error("Erro get tecnico", err);
-      res.status(500).json({ erro: "Erro na query" });
-    } else {
-      if (results.length ==0) {
-	res.status(404).json({ erro: "User not found" });
-      } else {
-        res.status(200).json(results);
-      }
-    }
-  });
+	// req.params.id mapeia o :id que está no URL acima.
+	con.query(sql, [req.params.id], (err, results) => {
+		if (err) {
+			console.error("Erro get tecnico", err);
+			res.status(500).json({ erro: "Erro na query" });
+		} else {
+			if (results.length ==0) {
+				res.status(404).json({ erro: "Tecnico not found" });
+			} else {
+				res.status(200).json(results);
+			}
+		}
+	});
 });
 
 //lista de materiais
 app.get("/materiais", (req, res) => {
-  let sql = "SELECT * FROM materiais";
+	let sql = "SELECT * FROM materiais";
 
-  con.query(sql, (err, results) => {
-    if (err) {
-      console.error("Erro get tecnicos", err);
-      res.status(500).json({ erro: "Erro na query" });
-    } else {
-      res.status(200).json(results);
-    }
-  });
+	con.query(sql, (err, results) => {
+		if (err) {
+			console.error("Erro get materiais", err);
+			res.status(500).json({ erro: "Erro na query" });
+		} else {
+			res.status(200).json(results);
+		}
+	});
 });
 
 //material (pelo ID)
 app.get("/materiais/id/:id", (req, res) => {
-  let sql = "SELECT * FROM materiais WHERE idMaterial = ?";
+	let sql = "SELECT * FROM materiais WHERE idMaterial = ?";
 
-  // req.params.id mapeia o :id que está no URL acima.
-  con.query(sql, [req.params.id], (err, results) => {
-    if (err) {
-      res.status(500).json({ erro: "Erro na query" });
-    } else {
-      if (results.length ==0) {
-	res.status(404).json({ erro: "User not found" });
-      } else {
-        res.status(200).json(results);
-      }
-    }
-  });
+	// req.params.id mapeia o :id que está no URL acima.
+	con.query(sql, [req.params.id], (err, results) => {
+		if (err) {
+			res.status(500).json({ erro: "Erro na query" });
+		} else {
+			if (results.length ==0) {
+				res.status(404).json({ erro: "Material not found" });
+			} else {
+				res.status(200).json(results);
+			}
+		}
+	});
 });
 
 
 
 //lista de procedimentos
 app.get("/procedimentos", (req, res) => {
-  let sql = "SELECT * FROM procedimentos";
+	let sql = "SELECT * FROM procedimentos";
 
-  con.query(sql, (err, results) => {
-    if (err) {
-      console.error("Erro get tecnicos", err);
-      res.status(500).json({ erro: "Erro na query" });
-    } else {
-      res.status(200).json(results);
-    }
-  });
+	con.query(sql, (err, results) => {
+		if (err) {
+			console.error("Erro get procedimentos", err);
+			res.status(500).json({ erro: "Erro na query" });
+		} else {
+			res.status(200).json(results);
+		}
+	});
 });
 
 //procedimento (pelo ID)
 app.get("/procedimento/id/:id", (req, res) => {
-  let sql = "SELECT * FROM procedimentos WHERE idProcedimento = ?";
+	let sql = "SELECT * FROM procedimentos WHERE idProcedimento = ?";
 
-  // req.params.id mapeia o :id que está no URL acima.
-  con.query(sql, [req.params.id], (err, results) => {
-    if (err) {
-      res.status(500).json({ erro: "Erro na query" });
-    } else {
-      if (results.length ==0) {
-	res.status(404).json({ erro: "User not found" });
-      } else {
-        res.status(200).json(results);
-      }
-    }
-  });
+	// req.params.id mapeia o :id que está no URL acima.
+	con.query(sql, [req.params.id], (err, results) => {
+		if (err) {
+			res.status(500).json({ erro: "Erro na query" });
+		} else {
+			if (results.length ==0) {
+				res.status(404).json({ erro: "Procedimento not found" });
+			} else {
+				res.status(200).json(results);
+			}
+		}
+	});
 });
 
 //lista de processosObra
 app.get("/processosObra", (req, res) => {
-  let sql = "SELECT * FROM processosObra";
+	let sql = "SELECT * FROM processosObra";
 
-  con.query(sql, (err, results) => {
-    if (err) {
-      console.error("Erro get tecnicos", err);
-      res.status(500).json({ erro: "Erro na query" });
-    } else {
-      res.status(200).json(results);
-    }
-  });
+	con.query(sql, (err, results) => {
+		if (err) {
+			console.error("Erro get processosObra", err);
+			res.status(500).json({ erro: "Erro na query" });
+		} else {
+			res.status(200).json(results);
+		}
+	});
 });
 
 //processosObra (pelo ID)
 app.get("/processosObra/id/:id", (req, res) => {
-  let sql = "SELECT * FROM processosObra WHERE numProcesso = ?";
+	let sql = "SELECT * FROM processosObra WHERE numProcesso = ?";
 
-  // req.params.id mapeia o :id que está no URL acima.
-  con.query(sql, [req.params.id], (err, results) => {
-    if (err) {
-      res.status(500).json({ erro: "Erro na query" });
-    } else {
-      if (results.length ==0) {
-	res.status(404).json({ erro: "User not found" });
-      } else {
-        res.status(200).json(results);
-      }
-    }
-  });
+	// req.params.id mapeia o :id que está no URL acima.
+	con.query(sql, [req.params.id], (err, results) => {
+		if (err) {
+			res.status(500).json({ erro: "Erro na query" });
+		} else {
+			if (results.length ==0) {
+				res.status(404).json({ erro: "ProcessoObra not found" });
+			} else {
+				res.status(200).json(results);
+			}
+		}
+	});
 });
 
 //lista de processos
 app.get("/processos", (req, res) => {
-  let sql = "SELECT * FROM processos";
+	let sql = "SELECT * FROM processos";
 
-  con.query(sql, (err, results) => {
-    if (err) {
-      console.error("Erro get tecnicos", err);
-      res.status(500).json({ erro: "Erro na query" });
-    } else {
-      res.status(200).json(results);
-    }
-  });
+	con.query(sql, (err, results) => {
+		if (err) {
+			console.error("Erro get processos", err);
+			res.status(500).json({ erro: "Erro na query" });
+		} else {
+			res.status(200).json(results);
+		}
+	});
 });
 
 //processo (pelo ID)
 app.get("/processos/id/:id", (req, res) => {
-  let sql = "SELECT * FROM processos WHERE idProcesso = ?";
+	let sql = "SELECT * FROM processos WHERE idProcesso = ?";
 
-  // req.params.id mapeia o :id que está no URL acima.
-  con.query(sql, [req.params.id], (err, results) => {
-    if (err) {
-      res.status(500).json({ erro: "Erro na query" });
-    } else {
-      if (results.length ==0) {
-	res.status(404).json({ erro: "User not found" });
-      } else {
-        res.status(200).json(results);
-      }
-    }
-  });
+	// req.params.id mapeia o :id que está no URL acima.
+	con.query(sql, [req.params.id], (err, results) => {
+		if (err) {
+			res.status(500).json({ erro: "Erro na query" });
+		} else {
+			if (results.length ==0) {
+				res.status(404).json({ erro: "Processo not found" });
+			} else {
+				res.status(200).json(results);
+			}
+		}
+	});
+});
+
+//tecnicos de um processo
+app.get("/processos/:id/tecnicos", (req, res) => {
+	let sql = "SELECT tec.* FROM tecnicoProcesso, tecnicos tec WHERE tecnico = idTecnico and processo = ?";
+
+	// req.params.id mapeia o :id que está no URL acima.
+	con.query(sql, [req.params.id], (err, results) => {
+		if (err) {
+			console.error("Erro get tecnicos do processo", err);
+			res.status(500).json({ erro: "Erro na query" });
+		} else {
+			if (results.length ==0) {
+				res.status(404).json({ erro: "Tecnicos not found" });
+			} else {
+				res.status(200).json(results);
+			}
+		}
+	});
+});
+//lista de objetos
+app.get("/objetos", (req, res) => {
+	let sql = "SELECT * FROM objetos";
+
+	con.query(sql, (err, results) => {
+		if (err) {
+			console.error("Erro get objetos", err);
+			res.status(500).json({ erro: "Erro na query" });
+		} else {
+			res.status(200).json(results);
+		}
+	});
+});
+
+//objeto (pelo ID)
+app.get("/objetos/id/:id", (req, res) => {
+	let sql = "SELECT * FROM objetos WHERE idObjeto = ?";
+
+	// req.params.id mapeia o :id que está no URL acima.
+	con.query(sql, [req.params.id], (err, results) => {
+		if (err) {
+			res.status(500).json({ erro: "Erro na query" });
+		} else {
+			if (results.length ==0) {
+				res.status(404).json({ erro: "Objeto not found" });
+			} else {
+				res.status(200).json(results);
+			}
+		}
+	});
 });
 
 app.listen(8080);
