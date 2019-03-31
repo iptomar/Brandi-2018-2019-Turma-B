2a6aac42512908479b2694de1ba8ffb86ecfa731
sandyvmarques <aluno19997@ipt.pt>
2019-03-31 02:00:44 +0100

    Associação da tabela na página de perfil

---
+++ Frontend/Pag_Perfil-RutePaz/app/src/Paginas/HomePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+
+class HomePage extends Component {
+	render() {
+		return (
+			<div className="Home-grid ">
+				<h1>Bem Vindo!</h1>
+				<p>Laboratório de Conservação e Restauro</p>
+			</div>
+		);
+	}
+}
+export default HomePage;
---
+++ Frontend/Pag_Perfil-RutePaz/app/src/Paginas/Projectos.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Projectos extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Projectos</h1>
+			</div>
+		);
+	}
+}
+export default Projectos;
