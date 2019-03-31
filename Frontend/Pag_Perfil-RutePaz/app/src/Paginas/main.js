3587f522c36c411c144d617e7db8b6d2fe7fad8a
sandyvmarques <aluno19997@ipt.pt>
2019-03-31 02:01:58 +0100

    Criação de Rotas para outras Paginas (Home,Fichas Técnicas, etc)

---
+++ Frontend/Pag_Perfil-RutePaz/app/src/Paginas/main.js
@@ -0,0 +1,20 @@
+/**
+ * Criacao de todas as Rotas
+ */
+import React from "react";
+import PP from "./PP";
+import Projectos from "./Projectos";
+import FichaTecnicos from "./FichaTecnicos";
+import HomePage from "./HomePage";
+
+import { Switch, Route } from "react-router-dom";
+
+const Main = () => (
+	<Switch>
+		<Route exact path="/" component={HomePage} />
+		<Route path="/Projectos" component={Projectos} />
+		<Route path="/FichaTecnicos" component={FichaTecnicos} />
+		<Route path="/PP" component={PP} />
+	</Switch>
+);
+export default Main;
