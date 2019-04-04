ae1ab71cf93764792b680f3d6046ad4bcdcfb56b
Edgar Oliveira <aluno19987@ipt.pt>
2019-04-04 11:53:03 +0100

    Correção de erros
    Correção de erros dos métodos update e insert da ficha tecnica

--- Backend/NodeJS/ficha tecnica.js
+++ Backend/NodeJS/ficha tecnica.js
@@ -28,10 +28,10 @@ app.post('/inserirFT', function(request, response) {
                 idTecnico=results[0].idTecnico;
 				
 				con.query('SELECT * FROM objetos WHERE designacao = ?', [objeto],
-				function(error, results, fields) {;
+				function(error, results, fields) {
 					//objeto encontrado
-					idObjeto=results[0].idObjeto;
 					if (results.length > 0) {
+						idObjeto=results[0].idObjeto;
 						con.query('SELECT * FROM processos WHERE objeto=?', [idObjeto],
 						function(error, results, fields) {
 							if (results.length == 0) {
@@ -40,8 +40,8 @@ app.post('/inserirFT', function(request, response) {
 								con.query('INSERT INTO processos SET ?', [processo],
 								function(error, results, fields) {
 									if(error){
-										response.send("erro1");
-										response.end();
+										//response.send("erro1");
+										//response.end();
 									}else{
 										idProcesso=results.insertId;
 										//insere na tabela tecnicoProcesso os dados
@@ -51,6 +51,7 @@ app.post('/inserirFT', function(request, response) {
 											if(error){
 												 response.send("erro2");
 											}else{
+												response.send("done")
 												response.end();
 											}
 	
@@ -60,32 +61,29 @@ app.post('/inserirFT', function(request, response) {
 							}
 							else{
 								response.send('Ficha tecnica já existe.');
-								 response.end();
+								response.end();
 							}
 						});	
 					//objeto não encontrado
 					} else {
 						response.send('Objeto inválido.');
-						 response.end();
+						response.end();
 					}
 				});
 
 
             //utilizador não encontrado
             } else {
-               response.send('Coordenador inválido.');
+                response.send('Coordenador inválido.');
  		response.end();
             }
         });
-        response.end();
     //não foram recebidos dados
     } else {
-	
         response.send('Por favor peencha todos os campos.');
         response.end();
     }
 });
-
 
 
 //method: post | action: updateFT
@@ -115,41 +113,52 @@ app.post('/objeto/:id/updateFT', function(request, response) {
 			//utilizador encontrado
 			if (results.length > 0) {
 				idTecnico=results[0].idTecnico;
+				//verifica se existe a ficha tecnica
+				const tecnicoProcesso = {tecnico:idTecnico, funcao:funcao }
+				con.query('select * from processos WHERE objeto = ?', [request.params.id],
+				function(error, results, fields) {
+					if (error) {
+						response.send( "Erro na query1" );
+						response.end();
+					} else {
+						if(results.length>0){
+							//update na tabela processo dos dados
+							const processo= {LCRM:LCRM, CEARC:CEARC, dataAberturaLCRM:dataAberturaLCRM, dataAberturaCEARC:dataAberturaCEARC, dataEntradaLCRM:dataEntradaLCRM, dataEntradaCEARC:dataEntradaCEARC}
+							con.query('UPDATE processos SET  ? WHERE objeto = ?', [processo, request.params.id],
+							function(error, results, fields) {
+								if (error) {
+									response.send( "Erro na query" );
+									response.end();
+								} else {
+									//update na tabela tecnicoProcesso dos dados
+									const tecnicoProcesso = {tecnico:idTecnico, funcao:funcao }
+									con.query('update tecnicoProcesso SET ? WHERE processo=(SELECT idProcesso FROM processos WHERE objeto = ?)', [tecnicoProcesso,request.params.id],
+									function(error, results, fields) {
+										if (error) {
+											response.send( "Erro na query" );
+											response.end();
+										} else {
+											response.send("ficha tecnica alterada com sucesso");
+											response.end();
+										}	
+									});
+								}
+							});
+						}else{
+						response.send("ficha tecnica not found");
+						response.end();
+						
+						}
+					}	
+				});
+
+				
 			//utilizador não encontrado
 			} else {
 				response.send('Coordenador inválido.');
+				response.end();
 			}		
 		});
-		
-		//update na tabela processo dos dados
-		const processo= {LCRM:LCRM, CEARC:CEARC, dataAberturaLCRM:dataAberturaLCRM, dataAberturaCEARC:dataAberturaCEARC, dataEntradaLCRM:dataEntradaLCRM, dataEntradaCEARC:dataEntradaCEARC}
-		con.query('UPDATE processos SET  ? WHERE objeto = ?', [processo, req.params.id],
-		function(error, results, fields) {
-			if (err) {
-				res.status(500).json({ erro: "Erro na query" });
-			} else {
-				if (results.length ==0) {
-					res.status(404).json({ erro: "Ficha Tecnica not found" });
-				}
-			}
-		});
-		
-		
-		//update na tabela tecnicoProcesso dos dados
-		const tecnicoProcesso = {tecnico:idTecnico, funcao:funcao }
-		con.query('update tecnicoProcesso SET = ? WHERE processo=(SELECT idProcesso FROM processos WHERE objeto = ?)', [tecnicoProcesso,req.params.id],
-		function(error, results, fields) {
-			if (err) {
-				res.status(500).json({ erro: "Erro na query" });
-			} else {
-				if (results.length ==0) {
-					res.status(404).json({ erro: "Ficha Tecnica not found" });
-				} else {
-					res.status(200).json(results);
-				}
-			}	
-		});
-		response.end();
 	//não foram recebidos dados
 	} else {
 		response.send('Por favor peencha todos os campos.');
@@ -157,6 +166,8 @@ app.post('/objeto/:id/updateFT', function(request, response) {
 	}
 });
 
+//method: get | action: consultarFT
+//metodo que permite consultar uma ficha tecnica
 app.get("/objeto/:id/consultarFT", (req, res) => {
 	let sql = "Select o.designacao, p.LCRM, p.CEARC, p.dataAberturaLCRM, p.dataAberturaCEARC, p.dataEntradaLCRM, p.dataEntradaCEARC, t.nome, tp.funcao  from processos p, tecnicos t, tecnicoProcesso tp, objetos o where t.idTecnico=tp.tecnico and p.idProcesso=tp.processo and p.objeto=o.idObjeto and p.objeto = ?";
 
