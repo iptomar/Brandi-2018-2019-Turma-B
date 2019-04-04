7bc0b7b1806fb0e92c5146b28a9019dc4d4ef770
Diogo Ribeiro <aluno20167@ipt.pt>
2019-04-04 01:07:42 +0100

    Update login.js

--- Backend/NodeJS/login.js
+++ Backend/NodeJS/login.js
@@ -144,21 +144,27 @@ app.get("/tecnicos/id/:id", (req, res) => {
 
 //tecnico (pelo username)
 app.get("/tecnicos/username/:username", (req, res) => {
-	let sql = "SELECT * FROM tecnicos WHERE username = ?";
+	if(req.session.user){
+			let sql = "SELECT * FROM tecnicos WHERE username = ?";
 
-	// req.params.id mapeia o :username que está no URL acima.
-	con.query(sql, [req.params.username], (err, results) => {
-		if (err) {
-			console.error("Erro get tecnico", err);
-			res.status(500).json({ erro: "Erro na query" });
-		} else {
-			if (results.length ==0) {
-				res.status(404).json({ erro: "Tecnico not found" });
+		// req.params.id mapeia o :username que está no URL acima.
+		con.query(sql, [req.params.username], (err, results) => {
+			if (err) {
+				console.error("Erro get tecnico", err);
+				res.status(500).json({ erro: "Erro na query" });
 			} else {
-				res.status(200).json(results);
+				if (results.length ==0) {
+					res.status(404).json({ erro: "Tecnico not found" });
+				} else {
+					res.status(200).json(results);
+				}
 			}
-		}
-	});
+		});
+	}
+	else{
+		res.status(500).json({ erro: "Erro Hello" });
+	}
+	
 });
 
 //lista de materiais
