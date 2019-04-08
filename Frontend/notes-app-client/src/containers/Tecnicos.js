ebfff0efc8dd80ffd03e7bed6343fc240beb2890
Andre Fronteira <aluno20160@ipt.pt>
2019-04-09 00:53:08 +0100

    Update Tecnicos.js

--- Frontend/notes-app-client/src/containers/Tecnicos.js
+++ Frontend/notes-app-client/src/containers/Tecnicos.js
@@ -13,89 +13,80 @@ export default class Tecnicos extends Component {
   }
 
 componentDidMount(){
-	if(window.location.pathname === "/tecnicos"){
+	let main = document.getElementById("main")
 
-		let main = document.getElementById("main")
+	let divInic = document.createElement("div");
+	divInic.id = "grid";
+	divInic.className = "container"
 
-		let divInic = document.createElement("div");
-		divInic.id = "grid";
-		divInic.className = "container"
+	let divSec = document.createElement("div");
+	divSec.id = "row1";
+	divSec.className = "row"
 
-		let divSec = document.createElement("div");
-		divSec.id = "row1";
-		divSec.className = "row"
-
-		divInic.appendChild(divSec);
-		main.appendChild(divInic);
-		
-		//const proxyurl = "http://cors-anywhere.herokuapp.com/";
-		axios.get(/*proxyurl + 'http://brandi.ipt.pt*/'/api/tecnicos/')
-		.then((response) => {
-			return response.data;
-		})
-		.then(data => {
-			let propsHistory = this.props.history;
-			for (var i = 1; i < data.length+1; i++) {
-				
-				// um objeto do array
-				let user = data[i-1];
-				
-				// designação do objeto
-				let nome = user.nome;
-				
-				let deck = document.querySelector('#row1');
-				deck.classList.add("deckObj");
-				
-				let col = document.createElement('div');
-				col.classList.add('col');
-				col.classList.add('colObj');
-				col.classList.add("col-xs-12");
-				col.classList.add("col-sm-6");
-				col.classList.add("col-md-4");
-				col.classList.add("col-lg-3");
-				col.classList.add("d-flex");
-				col.classList.add("align-items-stretch");
-				deck.appendChild(col);
-
-				let card = document.createElement('div');
-				card.id = user.username
-				card.classList.add('card');
-				card.classList.add('cardObj');
-				card.classList.add("m-3");
-				card.addEventListener('click', function(){
-					propsHistory.push("/tecnicos/"+this.id);
-				});
-				col.appendChild(card);
-				
-				let image = document.createElement('img');
-				image.setAttribute('alt','alt');
-				image.classList.add('card-img');
-				image.classList.add('imgObj');
-				image.classList.add('img-fluid');
-				card.appendChild(image);
+	divInic.appendChild(divSec);
+	main.appendChild(divInic);
+	
+	//const proxyurl = "http://cors-anywhere.herokuapp.com/";
+	axios.get(/*proxyurl + 'http://brandi.ipt.pt*/'/api/tecnicos/')
+	.then((response) => {
+		return response.data;
+	})
+	.then(data => {
+		let propsHistory = this.props.history;
+		for (var i = 1; i < data.length+1; i++) {
 			
-				let body = document.createElement('div');
-				body.classList.add('card-body');
-				body.classList.add('bodyObj');
-				card.appendChild(body);
+			// um objeto do array
+			let user = data[i-1];
+			
+			// designação do objeto
+			let nome = user.nome;
+			
+			let deck = document.querySelector('#row1');
+			deck.classList.add("deckObj");
+			
+			let col = document.createElement('div');
+			col.classList.add('col');
+			col.classList.add('colObj');
+			col.classList.add("col-xs-12");
+			col.classList.add("col-sm-6");
+			col.classList.add("col-md-4");
+			col.classList.add("col-lg-3");
+			col.classList.add("d-flex");
+			col.classList.add("align-items-stretch");
+			deck.appendChild(col);
 
-				let titulo = document.createElement('div');
-				titulo.classList.add('card-title');
-				titulo.classList.add('titleObj');
-				titulo.textContent = nome;
-				body.appendChild(titulo);
-			}
-		})
-		.catch(err => {
+			let card = document.createElement('div');
+			card.id = user.username
+			card.classList.add('card');
+			card.classList.add('cardObj');
+			card.classList.add("m-3");
+			card.addEventListener('click', function(){
+				propsHistory.push("/profile/" + this.id);
+			});
+			col.appendChild(card);
+			
+			let image = document.createElement('img');
+			image.setAttribute('alt','alt');
+			image.classList.add('card-img');
+			image.classList.add('imgObj');
+			image.classList.add('img-fluid');
+			card.appendChild(image);
+		
+			let body = document.createElement('div');
+			body.classList.add('card-body');
+			body.classList.add('bodyObj');
+			card.appendChild(body);
 
-		});
-	}else{
-		let userName = (window.location.pathname).split("/")[2];
+			let titulo = document.createElement('div');
+			titulo.classList.add('card-title');
+			titulo.classList.add('titleObj');
+			titulo.textContent = nome;
+			body.appendChild(titulo);
+		}
+	})
+	.catch(err => {
 
-		this.props.history.push("/profile" + userName);
-	}  
-
-
+	});
 	
 }
 	render() {
