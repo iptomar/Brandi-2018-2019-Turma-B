976d610dc28af4020d67589166d2c00467d5d156
Diogo Ribeiro <aluno20167@ipt.pt>
2019-04-23 16:40:13 +0100

    Update auth.js
    Co-Authored-By: miguelangelotome <miguelangelotome@users.noreply.github.com>

--- Backend/NodeJS/modules/auth.js
+++ Backend/NodeJS/modules/auth.js
@@ -1,5 +1,7 @@
 ﻿//modules
 var session = require('express-session')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 //export
 module.exports = function(app, con, verificaLogin, verificaLoginAdmin) {
 
