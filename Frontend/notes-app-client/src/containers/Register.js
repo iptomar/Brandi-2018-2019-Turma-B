00e6acf82e94b98f566d7a99c888989a3214a788
Diogo Ribeiro <aluno20167@ipt.pt>
2019-04-28 21:01:03 +0100

    Frontend pagina register
    Co-Authored-By: Edgar Oliveira <aluno19987@users.noreply.github.com>

--- Frontend/notes-app-client/src/containers/Register.js
+++ Frontend/notes-app-client/src/containers/Register.js
@@ -57,104 +57,107 @@ export default class Login extends Component {
   }
   render(){
     return (
-      <div>
-        <FormGroup className="FormField">
-        <ControlLabel className="FormField__Label" htmlFor="name">
-          Username
-        </ControlLabel>
-        <FormControl
-          type="username"
-          id="username"
-          className="FormField__Input"
-          placeholder="Insira o username"
-          name="username"
-          onChange={this.handleChange}
-        />
-      </FormGroup>
+      <div class="bodydiv">
+          <div class="registerform">
+          
+            <FormGroup className="FormField Fielddiv">
+            <ControlLabel className="FormField__Label" htmlFor="name">
+              Username
+            </ControlLabel>
+            <FormControl
+              type="username"
+              id="username"
+              className="FormField__Input regField"
+              placeholder="Insira o username"
+              name="username"
+              onChange={this.handleChange}
+            />
+          </FormGroup>
 
-      <FormGroup className="FormField">
-        <ControlLabel className="FormField__Label">
-          password
-        </ControlLabel>
-        <FormControl
-          type="password"
-          id="password"
-          className="FormField__Input"
-          placeholder="Insira a sua password"
-          name="password"
-          onChange={this.handleChange}
-        />
-        </FormGroup>
+          <FormGroup className="FormField Fielddiv">
+            <ControlLabel className="FormField__Label">
+              password
+            </ControlLabel>
+            <FormControl
+              type="password"
+              id="password"
+              className="FormField__Input regField"
+              placeholder="Insira a sua password"
+              name="password"
+              onChange={this.handleChange}
+            />
+            </FormGroup>
 
-        <FormGroup className="FormField">
-        <ControlLabel className="FormField__Label" htmlFor="email">
-          Email
-        </ControlLabel>
-        <FormControl
-          type="email"
-          id="email"
-          className="FormField__Input"
-          placeholder="Insira o seu email"
-          name="email"
-          onChange={this.handleChange}
-        />
-        </FormGroup>
+            <FormGroup className="FormField Fielddiv">
+            <ControlLabel className="FormField__Label" htmlFor="email">
+              Email
+            </ControlLabel>
+            <FormControl
+              type="email"
+              id="email"
+              className="FormField__Input regField"
+              placeholder="Insira o seu email"
+              name="email"
+              onChange={this.handleChange}
+            />
+            </FormGroup>
 
-        <FormGroup className="FormField">
-        <ControlLabel className="FormField__Label">
-          tipo
-        </ControlLabel>
-        <FormControl
-          id="tipo"
-          className="FormField__Input"
-          placeholder="Insira o seu tipo"
-          name="tipo"
-          onChange={this.handleChange}
-        />
-        </FormGroup>
+            <FormGroup className="FormField Fielddiv">
+            <ControlLabel className="FormField__Label">
+              tipo
+            </ControlLabel>
+            <FormControl
+              id="tipo"
+              className="FormField__Input regField"
+              placeholder="Insira o seu tipo"
+              name="tipo"
+              onChange={this.handleChange}
+            />
+            </FormGroup>
 
-        <FormGroup className="FormField">
-        <ControlLabel className="FormField__Label">
-          Habilitacoes
-        </ControlLabel>
-        <FormControl
-          id="Habilitacoes"
-          className="FormField__Input"
-          placeholder="Insira as suas Habilitacoes"
-          name="Habilitacoes"
-          onChange={this.handleChange}
-        />
-        </FormGroup>
+            <FormGroup className="FormField Fielddiv">
+            <ControlLabel className="FormField__Label">
+              Habilitacoes
+            </ControlLabel>
+            <FormControl
+              id="Habilitacoes"
+              className="FormField__Input regField"
+              placeholder="Insira as suas Habilitacoes"
+              name="Habilitacoes"
+              onChange={this.handleChange}
+            />
+            </FormGroup>
 
-        <FormGroup className="FormField">
-        <ControlLabel className="FormField__Label" htmlFor="email">
-          Nome
-        </ControlLabel>
-        <FormControl
-          id="Nome"
-          className="FormField__Input"
-          placeholder="Insira o seu Nome"
-          name="Nome"
-          onChange={this.handleChange}
-        />
-        </FormGroup>
+            <FormGroup className="FormField Fielddiv">
+            <ControlLabel className="FormField__Label" htmlFor="email">
+              Nome
+            </ControlLabel>
+            <FormControl
+              id="Nome"
+              className="FormField__Input regField"
+              placeholder="Insira o seu Nome"
+              name="Nome"
+              onChange={this.handleChange}
+            />
+            </FormGroup>
 
-        <FormGroup className="FormField">
-        <ControlLabel className="FormField__Label">
-          Nivel profissional
-        </ControlLabel>
-        <FormControl
-          id="NivProfissional"
-          className="FormField__Input"
-          placeholder="Insira o seu nivel profissional"
-          name="NivProfissional"
-          onChange={this.handleChange}
-        />
-        </FormGroup>
+            <FormGroup className="FormField Fielddiv">
+            <ControlLabel className="FormField__Label">
+              Nivel profissional
+            </ControlLabel>
+            <FormControl
+              id="NivProfissional"
+              className="FormField__Input regField"
+              placeholder="Insira o seu nivel profissional"
+              name="NivProfissional"
+              onChange={this.handleChange}
+            />
+            </FormGroup>
 
-        <button id="idBtn"className="FormField__Button mr-20" onClick = {this.handleSubmit}>
-          Registar
-        </button>
+            <button id="idBtn"className="FormField__Button mr-20 buttondiv" onClick = {this.handleSubmit}>
+              Registar
+            </button>
+          </div>
        </div>
     );
   }
