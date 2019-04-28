7592cdf8e6cd08371aa6a35fda541907a0e27001
RutePaz <aluno19996@ipt.pt>
2019-04-28 23:05:34 +0100

    Adição da página "Estado de Conservação" das Fichas Técnicas
    Co-Authored-By: Sandra Marques <sandyvmarques@users.noreply.github.com>

---
+++ Frontend/Fichas Técnicas/src/Paginas/EstadoConservacao.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+
+class CondicoesAmbientais extends Component {
+	render() {
+  return (
+    <div className="App">
+      <div className="right">
+      <h3>Deterioração Biológica dos Materiais</h3>
+        <p>Identificação de Patologias e Agentes de Biodeterioração - Diagnóstico</p>
+      <h3>Estrutura</h3>
+      <textarea>
+      </textarea>
+      <h3>Superfície</h3>
+      <textarea>
+      </textarea>
+      <h3>Elementos Acessórios</h3>
+      <textarea>
+      </textarea>
+      <h3>Observações|Conclusões</h3>
+      <textarea>
+      </textarea>
+      </div>
+      <div className="left">
+      <h1>Estado de Conservação</h1>
+      <h3>Deterioração Física, Química e Mecânica dos Materiais:</h3>
+      <p>Alterabilidades: Decorrente de envelhecimento natural</p>
+      <p>Alteração: Decorrente de fatores físicos, químicos, biológicos e antrópicos</p>
+      <h3>Estrutura</h3>
+      <textarea>
+      </textarea>
+      <h3>Superfície</h3>
+      <textarea>
+      </textarea>
+      <h3>Elementos Acessórios</h3>
+      <textarea>
+      </textarea>
+
+       
+
+
+      </div>
+
+
+    </div>
+  );
+}
+}
+export default CondicoesAmbientais;
