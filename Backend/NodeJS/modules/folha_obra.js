afcb85d95216d771ceec64a46e34991afba4d1dd
Edgar Oliveira <aluno19987@ipt.pt>
2019-05-15 09:39:00 +0100

    Correção dos métodos existentes
    Correção dos métodos existentes das folhas de obra para coincidir com o novo modelo de dados

--- Backend/NodeJS/modules/folha_obra.js
+++ Backend/NodeJS/modules/folha_obra.js
@@ -3,17 +3,17 @@ var session = require('express-session')
 //export
 module.exports = function(app, con, verificaLogin, verificaLoginAdmin) {
 	//metodo que permite consultar a lista de processos obra de um objeto
- 	app.get("/objetos/:id/processosObra", verificaLogin, (req, res) => {
-		let sql1 = "select * from processosObra where objeto = ?"
-		// req.params.id mapeia o :id que está no URL acima.
-		con.query(sql1 , [req.params.id], (err, results) => {
+ 	app.get("/pecas/:id/procedimentos", verificaLogin, (request, response) => {
+		let sql1 = "select * from procedimentos where peca = ?"
+		// request.params.id mapeia o :id que está no URL acima.
+		con.query(sql1 , [request.params.id], (err, results) => {
 			if (err) {
-				res.status(500).json({ erro: "Erro na query" })
+				responseres.status(500).json({ erro: "Erro na query" })
 			} else {
 				if (results.length == 0) {
-					res.status(404).json({ erro: "Folhas de obra não encontradas" })
+					response.status(404).json({ erro: "Procedimentos não encontrados" })
 				} else {					
-					res.status(200).json(results)
+					response.status(200).json(results)
 					console.log(JSON.stringify(results,null,2))
 				}
 			}
@@ -21,54 +21,42 @@ module.exports = function(app, con, verificaLogin, verificaLoginAdmin) {
 	})
 
 	//metodo que permite consultar uma folha de obra
-	app.get("/processosObra/:id", verificaLogin, (req, res) => {
-		let json=[
-		{
-			numProcesso:null,
-			designacao:null,
-			procedimentos:[]
-		}
-		]
-		let procedimentos={
+	app.get("/procedimento/:id", verificaLogin, (request, response) => {
+		let json=[{
 			idProcedimento: null,
 			data : null,
 			designacao:null,
 			duracao:null,
 			observacoes:null,
+			peca: null,
 			tecnico:null,
 			materiais:[]
-		}
-		let sql1 = "select * from processosObra where numProcesso = ?"
-		let sql2 = "select * from procedimentos where processoObra = ?"
-		let sql3 = "select * from materiais where procedimento=?"
-		// req.params.id mapeia o :id que está no URL acima.
-		con.query(sql1+';'+sql2 , [req.params.id,req.params.id], (err, results) => {
+		}]
+		let sql1 = "select * from procedimentos where idProcedimento = ?"
+		let sql2 = "select * from materiais where procedimento=?"
+		// request.params.id mapeia o :id que está no URL acima.
+		con.query(sql1 , [request.params.id], (err, results) => {
 			if (err) {
 				console.log(err)
-				res.status(500).json({ erro: "Erro na query" })
+				response.status(500).json({ erro: "Erro na query" })
 			} else {
-				if (results[0].length == 0) {
-					res.status(404).json({ erro: "Folha de obra não encontrada" })
-				} else {					
-					json[0].numProcesso=results[0].numProcesso
+				if (results.length == 0) {
+					response.status(404).json({ erro: "Procedimento não encontrado" })
+				} else {	
+					console.log(results)				
+					json[0].idProcedimento=results[0].idProcedimento
+					json[0].data=results[0].data
 					json[0].designacao=results[0].designacao
-					json[0].procedimentos=results[1]
-					for (let i = 0; i < results[1].length; i++) {
-						con.query(sql3, [results[1][i].idProcedimento],(err,results2)=>{
-							for(let j=0; j<json[0].procedimentos.length; j++){
-								console.log(i+"  "+j)
-								if(json[0].procedimentos[j].idProcedimento===results[1][i].idProcedimento){
-									console.log(results[1][i].idProcedimento)
-									console.log(results2)
-									json[0].procedimentos[j].materiais=results2
-									if( i === results[1].length-1 && j===json[0].procedimentos.length-1){
-										res.status(200).json(json)
-										console.log(JSON.stringify(json,null,2))
-									}
-								}
-							}
-						})
-					}
+					json[0].duracao=results[0].duracao
+					json[0].observacoes=results[0].observacoes
+					json[0].peca=results[0].peca
+					json[0].tecnico=results[0].tecnico
+					console.log(json)
+					con.query(sql2, [request.params.id],(err,results2)=>{
+						json[0].materiais=results2
+						response.status(200).json(json)
+						console.log(JSON.stringify(json,null,2))
+					})
 				}
 			}
 		})
