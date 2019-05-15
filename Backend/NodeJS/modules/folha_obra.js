f2fc80fa45dd74722acb50f5711d59bcbe95f594
Edgar Oliveira <aluno19987@ipt.pt>
2019-05-15 11:32:15 +0100

    webservices folha obra
    criação métodos de consultar, adicionar, alterar e remover
    
    Co-Authored-By: miguelangelotome <miguelangelotome@users.noreply.github.com>
    Co-Authored-By: Diogo Ribeiro <keiriko@users.noreply.github.com>

--- Backend/NodeJS/modules/folha_obra.js
+++ Backend/NodeJS/modules/folha_obra.js
@@ -2,62 +2,207 @@
 var session = require('express-session')
 //export
 module.exports = function(app, con, verificaLogin, verificaLoginAdmin) {
-	//metodo que permite consultar a lista de processos obra de um objeto
- 	app.get("/pecas/:id/procedimentos", verificaLogin, (request, response) => {
-		let sql1 = "select * from procedimentos where peca = ?"
-		// request.params.id mapeia o :id que está no URL acima.
-		con.query(sql1 , [request.params.id], (err, results) => {
-			if (err) {
-				responseres.status(500).json({ erro: "Erro na query" })
-			} else {
-				if (results.length == 0) {
-					response.status(404).json({ erro: "Procedimentos não encontrados" })
-				} else {					
-					response.status(200).json(results)
-					console.log(JSON.stringify(results,null,2))
-				}
-			}
-		})
-	})
-
+	
 	//metodo que permite consultar uma folha de obra
-	app.get("/procedimento/:id", verificaLogin, (request, response) => {
-		let json=[{
+	app.get("/peca/:id/folhaobra", verificaLogin, (req, res) => {
+		let json=[
+		]
+		let procedimentos={
 			idProcedimento: null,
 			data : null,
 			designacao:null,
 			duracao:null,
 			observacoes:null,
-			peca: null,
 			tecnico:null,
 			materiais:[]
-		}]
-		let sql1 = "select * from procedimentos where idProcedimento = ?"
+		}
+		let sql1 = "select * from procedimentos where peca = ?"
 		let sql2 = "select * from materiais where procedimento=?"
-		// request.params.id mapeia o :id que está no URL acima.
-		con.query(sql1 , [request.params.id], (err, results) => {
+		// req.params.id mapeia o :id que está no URL acima.
+		con.query(sql1 , [req.params.id], (err, results) => {
 			if (err) {
 				console.log(err)
-				response.status(500).json({ erro: "Erro na query" })
+				res.status(500).json({ erro: "Erro na query" })
 			} else {
-				if (results.length == 0) {
-					response.status(404).json({ erro: "Procedimento não encontrado" })
-				} else {	
-					console.log(results)				
-					json[0].idProcedimento=results[0].idProcedimento
-					json[0].data=results[0].data
-					json[0].designacao=results[0].designacao
-					json[0].duracao=results[0].duracao
-					json[0].observacoes=results[0].observacoes
-					json[0].peca=results[0].peca
-					json[0].tecnico=results[0].tecnico
-					console.log(json)
-					con.query(sql2, [request.params.id],(err,results2)=>{
-						json[0].materiais=results2
-						response.status(200).json(json)
-						console.log(JSON.stringify(json,null,2))
-					})
+				if (results[0].length == 0) {
+					res.status(404).json({ erro: "Folha de obra não encontrada" })
+				} else {
+					json=results
+					for (let i = 0; i < results.length; i++) {
+						con.query(sql2, [results[i].idProcedimento],(err,results2)=>{
+				
+							for(let j=0; j<json.length; j++){
+								if(json[j].idProcedimento===results[i].idProcedimento){
+									json[j].materiais=results2
+									if( i === results.length-1 && j===json.length-1){
+										res.status(200).json(json)
+										console.log(JSON.stringify(json,null,2))
+									}
+								}
+							}
+						})
+					}
 				}
+			}
+		})
+	})
+
+
+	//metodo que permite inserir um procedimento
+	app.post('/procedimentos/new', function(request, response) {
+		//guarda os dados recebidos
+		
+		let	designacao  = request.body.designacao
+		let	data  = request.body.data
+		let	duracao = request.body.duracao
+		let observacoes = request.body.observacoes
+		let	peca = request.body.peca
+		let	tecnico = request.body.tecnico
+		let materiais = request.body.materiais
+		console.log(materiais[0])
+		//foram recebidos dados
+		if (data && designacao && duracao && observacoes && peca && tecnico) {
+			sql0 = 'start transaction'
+			sql1 = 'insert into procedimentos (data, designacao, duracao, observacoes, peca, tecnico) values(?,?,?,?,?,?)'
+			sql2 = 'insert into materiais (material, quantidade, procedimento)values(?,?,?)'
+			con.query(sql0+';'+sql1, [data, designacao, duracao, observacoes, peca, tecnico],
+			function(error, results, fields) {
+				if(error){
+					console.log(error)
+					con.query('rollback')
+					response.send("Erro ao registar procedimento")
+					response.end()
+				} else {
+					let resposta = {
+						message: "Sucesso",
+						id: results[1].insertId 
+					}
+					for(let i=0; i<materiais.length; i++){
+						con.query(sql2, [materiais[i][0],materiais[i][0],resposta.id],
+						function(error, results, fields) {
+							if(error){
+								con.query('rollback')
+								response.send("Erro ao registar procedimento")
+								response.end()
+							}else{
+								if(i===materiais.length-1){
+									con.query('commit')
+									response.send(resposta)
+									response.end()
+								}
+							}
+						})
+					}
+				}
+			})
+		} else {
+			response.send("Dados incompletos")
+			response.end()
+		}
+	})
+
+	//metodo que permite alterar um procedimento
+	app.post('/procedimentos/:id/update', function(request, response) {
+		//guarda os dados recebidos	
+		let	designacao  = request.body.designacao
+		let	data  = request.body.data
+		let	duracao = request.body.duracao
+		let observacoes = request.body.observacoes
+		let	peca = request.body.peca
+		let	tecnico = request.body.tecnico
+		//foram recebidos dados
+		if (data && designacao && duracao && observacoes && peca && tecnico) {
+			sql0 = 'start transaction'
+			sql1 = 'update procedimentos set data = ?, designacao = ?, duracao = ?, observacoes = ?, peca = ?, tecnico = ? where idProcedimento = ?'
+			con.query(sql0+';'+sql1, [data, designacao, duracao, observacoes, peca, tecnico, request.params.id],
+			function(error, results, fields) {
+				if(error){
+					console.log(error)
+					con.query('rollback')
+					response.send("Erro ao alterar procedimento")
+					response.end()
+				} else {
+					con.query('commit')
+					response.send("Sucesso")
+					response.end()
+				}
+			})
+		} else {
+			response.send("Dados incompletos")
+			response.end()
+		}
+	})	
+
+	//metodo que permite alterar um procedimento
+	app.post('/materiais/:id/update', function(request, response) {
+		//guarda os dados recebidos	
+		let	materiais  = request.body.materiais
+		let	quantidade  = request.body.quantidade
+		//foram recebidos dados
+		if (materiais && quantidade) {
+			sql0 = 'start transaction'
+			sql1 = 'update materiais set material = ?, quantidade = ? where idMaterial = ?'
+			con.query(sql0+';'+sql1, [materiais, quantidade, request.params.id],
+			function(error, results, fields) {
+				if(error){
+					console.log(error)
+					con.query('rollback')
+					response.send("Erro ao alterar materiais")
+					response.end()
+				} else {
+					con.query('commit')
+					response.send("Sucesso")
+					response.end()
+				}
+			})
+		} else {
+			response.send("Dados incompletos")
+			response.end()
+		}
+	})	
+
+	//metodo que permite remover um material
+	app.get("/materiais/:id/remove", (request, response) => {
+		let sql1 = 'select * from materiais where idMaterial = ?'
+		// request.params.id mapeia o :id que está no URL acima.
+		con.query(sql1, [request.params.id], (err, results) => {
+			if(results.length<=0){
+				response.status(404).json({ erro: "Materiais não encontrado" })
+			}else{
+				let sql2 = 'delete from materiais where idMaterial = ?'
+				con.query(sql2, [request.params.id], (err, results) => {
+					if(err){
+						console.log(err)
+						response.status(500).json({ erro: "Erro ao remover material" });
+					} else {
+						response.status(200).json({message: "Material removido com sucesso"});
+					}
+				})
+			}
+		})
+	})
+
+	//metodo que permite remover um material
+	app.get("/procedimentos/:id/remove", (request, response) => {
+		let sql0 = 'select * from procedimentos where idProcedimento = ?'
+		// request.params.id mapeia o :id que está no URL acima.
+		con.query(sql0, [request.params.id], (err, results) => {
+			if(results.length<=0){
+				response.status(404).json({ erro: "Procedimento não encontrado" })
+			}else{
+				let sql1 = 'start transaction'
+				let sql2 = 'delete from materiais where procedimento = ?'
+				let sql3 = 'delete from procedimentos where idProcedimento=?'
+				con.query(sql1+';'+sql2+';'+sql3, [request.params.id,request.params.id], (err, results) => {
+					if(err){
+						console.log(err)
+						con.query('rollback')
+						response.status(500).json({ erro: "Erro ao remover material" });
+					} else {
+						con.query('commit')
+						response.status(200).json({message: "Material removido com sucesso"});
+					}
+				})
 			}
 		})
 	})
