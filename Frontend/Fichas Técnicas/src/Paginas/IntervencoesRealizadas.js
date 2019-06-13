847af622630ae5a4a444cd05a55cc31d2c1bf2a5
Simão Oliveira <symao96@gmail.com>
2019-06-13 15:02:30 +0100

    Adição de Bootstrap
    Correcção de Commit Anterior
    
    Co-Authored-By: joanaaalves <joanaaalves@users.noreply.github.com>
    Co-Authored-By: rafandre <rafandre@users.noreply.github.com>

--- Frontend/Fichas Técnicas/src/Paginas/IntervencoesRealizadas.js
+++ Frontend/Fichas Técnicas/src/Paginas/IntervencoesRealizadas.js
@@ -1,29 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-class IntervencoesRealizadas extends Component {
+export default class IntervencoesRealizadas extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="right">
-          <h3>Elementos Acessórios</h3>
-          <textarea cols="80" rows="3" />
-          <h2>Materias</h2>
-          <textarea cols="80" rows="3" />
 
-          <h3>Observações | Conclusões</h3>
-          <textarea cols="80" rows="3" />
-        </div>
-        <div className="left">
-          <h1>Intervenções Realizada</h1>
-          <h3>Estrutura</h3>
-          <textarea cols="80" rows="2" />
-          <h2>Materias</h2>
-          <textarea cols="80" rows="3" />
+      <div className="container">
+        <div className="row">
+          <div className="col col-md-6">
+            <h1>Intervenções Realizada</h1>
+            <h3 className="bg-secondary text-white">Estrutura</h3>
+            <div className="col">
+              <textarea className="form-control-2 ml-n4" cols="103" rows="5"></textarea>
+            </div>
+            <h2 className="font-weight-bold bg-secondary text-white">Materias</h2>
+            <div className="col">
+              <textarea className="form-control-2 ml-n4" cols="103" rows="5"></textarea>
+            </div>
+            <h3 className="mt-3 bg-secondary text-white">Superficie</h3>
+            <div className="col">
+              <textarea className="form-control-2 ml-n4" cols="103" rows="5"></textarea>
+            </div>
+            <h2 className="font-weight-bold bg-secondary text-white">Materias</h2>
+            <div className="col">
+              <textarea className="form-control-2 ml-n4" cols="103" rows="5"></textarea>
+            </div>
+          </div>
+          <div className="col col-md-6 mt-5">
+            <h3 className="mt-2 bg-secondary text-white">Elementos Acessórios</h3>
+            <div className="col">
+              <textarea className="form-control-2 ml-n4" cols="103" rows="7"></textarea>
+            </div>
+            <h2 className="font-weight-bold bg-secondary text-white">Materias</h2>
+            <div className="col">
+              <textarea className="form-control-2 ml-n4" cols="103" rows="7"></textarea>
+            </div>
 
-          <h3>Superficie</h3>
-          <textarea cols="80" rows="2" />
-          <h2>Materias</h2>
-          <textarea cols="80" rows="3" />
+            <h3 className="mt-3 bg-secondary text-white">Observações | Conclusões</h3>
+            <div className="col">
+              <textarea className="form-control-2 ml-n4" cols="103" rows="8"></textarea>
+            </div>
+          </div>
+
         </div>
       </div>
     );
